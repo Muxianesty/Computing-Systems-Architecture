e84842ecba6103205871440894df25816534fcf8
Muxianesty <71048120+Muxianesty@users.noreply.github.com>
2021-10-04 17:24:31 +0300

    Add files via upload

---
+++ First/FirstProject/cmake-build-debug/CMakeFiles/FirstProject.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Archive/First/FirstProject/Plant-entity classes/Bush.cpp" "C:/Archive/First/FirstProject/cmake-build-debug/CMakeFiles/FirstProject.dir/Plant-entity_classes/Bush.cpp.obj"
+  "C:/Archive/First/FirstProject/Plant-entity classes/Flower.cpp" "C:/Archive/First/FirstProject/cmake-build-debug/CMakeFiles/FirstProject.dir/Plant-entity_classes/Flower.cpp.obj"
+  "C:/Archive/First/FirstProject/Plant-entity classes/Plant.cpp" "C:/Archive/First/FirstProject/cmake-build-debug/CMakeFiles/FirstProject.dir/Plant-entity_classes/Plant.cpp.obj"
+  "C:/Archive/First/FirstProject/Plant-entity classes/Tree.cpp" "C:/Archive/First/FirstProject/cmake-build-debug/CMakeFiles/FirstProject.dir/Plant-entity_classes/Tree.cpp.obj"
+  "C:/Archive/First/FirstProject/PlantContainer.cpp" "C:/Archive/First/FirstProject/cmake-build-debug/CMakeFiles/FirstProject.dir/PlantContainer.cpp.obj"
+  "C:/Archive/First/FirstProject/main.cpp" "C:/Archive/First/FirstProject/cmake-build-debug/CMakeFiles/FirstProject.dir/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
